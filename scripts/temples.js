5ec7b99e369ef0ea8948b727ff13ccbde3dbd9c6
wesleyfigueira <wesleyfigueira1993@gmail.com>
2025-03-12 18:47:16 -0300

    js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,4 @@
+
 function showMenu() {
     let menuMobile = document.querySelector(".nav-list");
 
@@ -9,3 +10,15 @@ function showMenu() {
         document.querySelector('.icon').src = "images/close_white_36dp.svg";
     }
 }
+
+
+
+
+// Get the current year for copyright
+const currentYear = new Date().getFullYear();
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Output the last modified date
+    const lastModifiedParagraph = document.querySelector('footer p:nth-child(2)');
+    lastModifiedParagraph.textContent = `Last modified: ${document.lastModified} © ${currentYear}`;
+});
